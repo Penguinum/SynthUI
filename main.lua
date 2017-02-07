25c366ab3e96660310a76cab13521dab5b002212
fedor-elizarov <blogdron@gmail.com>
2017-02-07 15:29:56 +0300

    fix main

--- main.lua
+++ main.lua
@@ -1,15 +1,7 @@
 local SynthUI = require "SynthUI"
-
 local mainPanel = SynthUI.Panel:new():set{left=1, top=1}
-
-
-
 local polyline_edit = SynthUI.PolylineEdit:new():set{left=0, top=0}
-
-
-mainPanel:addNodes{button, polyline_edit}
-
-
+mainPanel:addNodes{polyline_edit}
 function love.draw()
   mainPanel:draw()
 end
